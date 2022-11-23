2f65d7cc1ec30547e122fd50f381e426ed7e3398
Tyler Wolf <SecWolf@proton.me>
2022-11-22 23:53:49 -0500

    Removed MEL functions. Currently works roughly

--- main.py
+++ main.py
@@ -1,103 +1,128 @@
 import os
 import csv
+import tarfile
+import fnmatch
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-# imports for converting WAV to MEL Spectograms
-import librosa
-import librosa.display
-import IPython.display as ipd
-import matplotlib.pyplot as plt
-
 AUDIO_DIR = "./recordings/"
-CHUNKS_DIR = "./chunks/"
-TRANS_DIR = "./transcripts/"
-MEL_DIR = "./mel/"
-
-
-# def package_dataset():
+CHUNKS_DIR = "./output/chunks/"
+TRANS_DIR = "./output/transcripts/"
+FILELIST_DIR = './output/filelists/'
+MEL_DIR = "./output/mel/"
 
 def chunk_audio():
     """
-        Chunks audio by pauses in speech pattern and outputs ####.wav files
+        Chunks audio by pauses in speech pattern and outputs @@###-####.wav files
     """
     
     if not os.path.exists(AUDIO_DIR):
         os.mkdir(AUDIO_DIR)
-    else:
-        recordings = sorted(os.listdir(AUDIO_DIR))
 
-        for i, filename in enumerate(recordings):
-            chunks = []
-            f = os.path.join(AUDIO_DIR, filename)
-            source_aud = AudioSegment.from_file(f)
-            chunks = chunks + split_on_silence(source_aud, min_silence_len=875, silence_thresh=-60)
-            for j, chunk in enumerate(chunks):
-                chunk_name = "JP" + f"{i+1}".zfill(3) + "-" + f"{j+1}".zfill(4) + ".wav"
-                chunk.export(CHUNKS_DIR + chunk_name, format="wav")
+    recordings = sorted(os.listdir(AUDIO_DIR))
+
+    for i, filename in enumerate(recordings):
+        print(f"Creating chunks from source file {i}....")
+        chunks = []
+        f = os.path.join(AUDIO_DIR, filename)
+        source_aud = AudioSegment.from_file(f)
+        
+        chunks = chunks + split_on_silence(source_aud, min_silence_len=375, silence_thresh=-40)
+        for j, chunk in enumerate(chunks):
+            chunk_name = "JP" + f"{i+1}".zfill(3) + "-" + f"{j+1}".zfill(4) + ".wav"
+            chunk.export(CHUNKS_DIR + chunk_name, format="wav")
 
 def transcribe_chunks():
     """
         Receives audio chunks and outputs transcriptions into CSV
     """
+
     if not os.path.exists(CHUNKS_DIR):
         os.mkdir(CHUNKS_DIR)
-    else:
-        with open(TRANS_DIR + 'metadata.csv', 'a') as file:
-            writer = csv.writer(file)
-            for filename in sorted(os.listdir(CHUNKS_DIR)):
-                f = os.path.join(CHUNKS_DIR, filename)
-                recog = sr.Recognizer()
-                with sr.AudioFile(f) as source:
-                    audio = recog.record(source)
-            
+
+    with open(TRANS_DIR + 'metadata.csv', 'a') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONE, quotechar='')
+        for filename in os.listdir(CHUNKS_DIR):
+            f = os.path.join(CHUNKS_DIR, filename)
+            recog = sr.Recognizer()
+            with sr.AudioFile(f) as source:
+                audio = recog.record(source)
+            try:
                 trans = recog.recognize_google(audio)
-                writer.writerow([f"{os.path.splitext(filename)[0]}|{trans}"])
+                writer.writerow([os.path.splitext(filename)[0] + '|' + trans])
+            except Exception:
+                print(f"****File --{filename}-- caused an error****")
+                os.remove(f)
+                print(f"****File --{filename}-- Removed from dir****")
 
-# def create_training_subsets():
-#     return
+def write_subset(file, data, x, y):
+    temp = data[x:x+y]
+    with open(file, 'w') as f:
+            for lines in temp:
+                f.write(f"{lines}\n")
 
-def wav_to_mel():
-    """
-        Convert WAV files into MEL Spectograms
-        Derived from musikalkemist AudioSignalProcessingForML Lessons
-        https://github.com/musikalkemist/AudioSignalProcessingForML
-    """
-    for filename in sorted(os.listdir(CHUNKS_DIR)):
-        f = os.path.join(CHUNKS_DIR, filename)
-        ipd.Audio(f)
+def create_training_subsets():
+    data = []
+    with open(f'{TRANS_DIR}/metadata.csv', 'r') as file:
+        data = file.read().splitlines()
+    data_length = len(data)
+    # if data_length < 5000:
+    #     print(f"{data_length} is not enough data to create valuable subsets")
+    # else:
+    write_subset('./output/filelists/jps_audio_text_test_filelist.txt', data, 0, 500)
+    write_subset('./output/filelists/jps_audio_text_train_filelist.txt', data, 500, data_length)
+    write_subset('./output/filelists/jps_audio_text_val_filelist.txt', data, 600, 100)
+    write_subset('./output/filelists/jps_audio_text_train_subset_64_filelist.txt', data, 700, 64)
+    write_subset('./output/filelists/jps_audio_text_train_subset_300_filelist.txt', data, 764, 300)
+    write_subset('./output/filelists/jps_audio_text_train_subset_625_filelist.txt', data, 1064, 625)
+    write_subset('./output/filelists/jps_audio_text_train_subset_1250_filelist.txt', data, 1689, 1250)
+    write_subset('./output/filelists/jps_audio_text_train_subset_2500_filelist.txt', data, 2939, 2500)
+    write_subset('./output/filelists/jps_mel_text_filelist.txt', data, 0, data_length)
+    write_subset('./output/filelists/jps_mel_text_test_filelist.txt', data, 0, 500)
+    write_subset('./output/filelists/jps_mel_text_train_filelist.txt', data, 500, data_length)
+    write_subset('./output/filelists/jps_mel_text_val_filelist.txt', data, 600, 100)
+    write_subset('./output/filelists/jps_mel_text_train_subset_64_filelist.txt', data, 700, 64)
+    write_subset('./output/filelists/jps_mel_text_train_subset_300_filelist.txt', data, 764, 300)
+    write_subset('./output/filelists/jps_mel_text_train_subset_625_filelist.txt', data, 1064, 625)
+    write_subset('./output/filelists/jps_mel_text_train_subset_1250_filelist.txt', data, 1689, 1250)
+    write_subset('./output/filelists/jps_mel_text_train_subset_2500_filelist.txt', data, 2939, 2500)
 
-        # load audio files with librosa
-        scale, sr = librosa.load(f)
+    data = []
+    for filename in os.listdir('./output/filelists/'):
+        if fnmatch.fnmatch(filename, '*audio*'):
+            with open(FILELIST_DIR+filename, 'r+') as file:
+                data = file.read().splitlines()
+                file.seek(0)
+                for line in data:
+                    file.write(f"JPSpeech-1.0/wavs/{line[:10]}.wav{line[10:]}\n")
+        elif fnmatch.fnmatch(filename, "*mels*"):
+            with open(FILELIST_DIR+filename, 'r+') as file:
+                data = file.read().splitlines()
+                file.seek(0)
+                for line in data:
+                    file.write(f"JPSpeech-1.0/mels/{line[:10]}.pt{line[10:]}\n")
+    # print(data[2:10])
 
-        filter_banks = librosa.filters.mel(n_fft=2048, sr=22050, n_mels=10)
-        filter_banks.shape
-
-        plt.figure(figsize=(25, 10))
-        librosa.display.specshow(filter_banks, 
-                                sr=sr, 
-                                x_axis="linear")
-        plt.colorbar(format="%+2.f")
-        #plt.show()
-
-        mel_spectrogram = librosa.feature.melspectrogram(scale, sr=sr, n_fft=2048, hop_length=512, n_mels=10)
-        mel_spectrogram.shape
-
-        log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
-        log_mel_spectrogram.shape
-        plt.figure(figsize=(25, 10))
-        librosa.display.specshow(log_mel_spectrogram, 
-                                x_axis="time",
-                                y_axis="mel", 
-                                sr=sr)
-        plt.colorbar(format="%+2.f")
-        plt.savefig(MEL_DIR + os.path.splitext(filename)[0] + '.png')
+def package_data():
+    with tarfile.open("./output/JPSpeech-1.0.tar.bz2", "w:bz2") as tar:
+        tar.add(CHUNKS_DIR, arcname=os.path.basename("./wavs"))
+        tar.add(TRANS_DIR, arcname=os.path.basename("/"))
+        tar.add(FILELIST_DIR, arcname=os.path.basename("./filelists"))
+        tar.close()
 
 def main():
     chunk_audio()
     transcribe_chunks()
-    #Wav_to_MEL()        
+    # Wav_to_MEL()
+    create_training_subsets()
+    package_data()
+    # data = []
+    # with open('./output/filelists/jps_audio_text_test_filelist.txt', 'r') as file:
+    #     data = file.read().splitlines()
+    # with open("./output/filelists/jps_audio_text_test_filelist.txt", "w") as file:
+    #     for line in data:
+    #         file.write(f"JPSpeech-1.0/mels/{line[:10]}.wav{line[10:]}\n")
 
 if __name__=="__main__":
     main()
