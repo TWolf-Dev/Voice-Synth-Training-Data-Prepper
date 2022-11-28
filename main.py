349fa9fe62ded7d515259f22571bf6fe526d2287
Tyler Wolf <SecWolf@proton.me>
2022-11-27 21:01:09 -0500

    Added docs

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+########################################
+#   Title: Voice Synth Training Data Prepper
+#   Author: Tyler Wolf
+#   Date: 11/11/2022
+#   
+#   Description:
+#       Creating datasets for my machine learning project was extremely
+#       tedious and error prone due to the large amount of files needed.
+#       This script was made to automate the audio chunking, transcribing,
+#       training data subset creation, and packaging process.
+#
+#   Use:
+#       All you need to do is place all your source audio files in the "Recordings"
+#       directory and run the script. Be sure to use as clean of audio as possible to
+#       minimize the likelyhood of unusable chunks or transcriptions.
+#
+#   Notes:
+#       Need to add logging and exception handling so the user knows where they are
+#       in terms of script progress.
+#       - Fix subset creation method, it's lacking.
+#       - Also see about optimizing chunking and transription process (try multi-threading). 
+#       - Add method for normalizing transcription (numbers, symbols, etc.)
+#       - Change audio chunk output to 22050 sample rate
+#
+#   My Project Stats:
+#       Script Runtime:                 2hr 47min
+#       Total Source Audio Files:       18
+#       Combined Source Audio Length:   13.18 hours
+#       Total Useable Chunks Created:   12,909
+#       Combined Chunk Audio Length:    11.01 hours
+#       Total Silence Cut:              128.26 mins
+#       Total Chunks Lost:              225 chunks
+#       Total Audio Duration Lost:      2.54 mins (Lost Chunks)
+#   
+########################################
+
 import os
 import csv
 import tarfile
@@ -37,6 +73,11 @@ def transcribe_chunks():
     """
         Receives audio chunks and outputs transcriptions into CSV
     """
+    ##
+    global files_lost
+    global total_audio_len
+    global audio_len_lost
+    ##
 
     if not os.path.exists(CHUNKS_DIR):
         os.mkdir(CHUNKS_DIR)
@@ -51,6 +92,7 @@ def transcribe_chunks():
             try:
                 trans = recog.recognize_google(audio)
                 writer.writerow([os.path.splitext(filename)[0] + '|' + trans])
+                total_audio_len = total_audio_len + AudioSegment.from_file(f).duration_seconds
             except Exception:
                 print(f"****File --{filename}-- caused an error****")
                 os.remove(f)
@@ -63,13 +105,15 @@ def write_subset(file, data, x, y):
                 f.write(f"{lines}\n")
 
 def create_training_subsets():
+    """
+        Create multiple testing, training, and validation subsets of gathered
+        transcriptions for mel and audio training
+    """
     data = []
     with open(f'{TRANS_DIR}/metadata.csv', 'r') as file:
         data = file.read().splitlines()
     data_length = len(data)
-    # if data_length < 5000:
-    #     print(f"{data_length} is not enough data to create valuable subsets")
-    # else:
+    
     write_subset('./output/filelists/jps_audio_text_test_filelist.txt', data, 0, 500)
     write_subset('./output/filelists/jps_audio_text_train_filelist.txt', data, 500, data_length)
     write_subset('./output/filelists/jps_audio_text_val_filelist.txt', data, 600, 100)
@@ -102,9 +146,11 @@ def create_training_subsets():
                 file.seek(0)
                 for line in data:
                     file.write(f"JPSpeech-1.0/mels/{line[:10]}.pt{line[10:]}\n")
-    # print(data[2:10])
 
 def package_data():
+    """
+        Place all output data into an organized compressed tar file
+    """
     with tarfile.open("./output/JPSpeech-1.0.tar.bz2", "w:bz2") as tar:
         tar.add(CHUNKS_DIR, arcname=os.path.basename("./wavs"))
         tar.add(TRANS_DIR, arcname=os.path.basename("/"))
@@ -112,17 +158,11 @@ def package_data():
         tar.close()
 
 def main():
+
     chunk_audio()
     transcribe_chunks()
-    # Wav_to_MEL()
     create_training_subsets()
     package_data()
-    # data = []
-    # with open('./output/filelists/jps_audio_text_test_filelist.txt', 'r') as file:
-    #     data = file.read().splitlines()
-    # with open("./output/filelists/jps_audio_text_test_filelist.txt", "w") as file:
-    #     for line in data:
-    #         file.write(f"JPSpeech-1.0/mels/{line[:10]}.wav{line[10:]}\n")
 
 if __name__=="__main__":
     main()
